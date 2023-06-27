be63125a0e383e6362724c5560d68471d1c5ec1b
Tom Rosenzweig <tom.rosenzweig@macio.de>
2023-06-27 15:08:26 +0200

    finish accordion

--- challenges/src/app/app.component.ts
+++ challenges/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { AccordionItem } from './components/accordion/accordion-item.interface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'challenges';
+
+  accordionItems: AccordionItem[] = [
+    { title: 'Example 1', content: 'Example Content 1', isExpanded: false },
+    { title: 'Example 2', content: 'Example Content 2', isExpanded: false },
+  ];
+
+  constructor() {}
 }
---
+++ challenges/src/app/components/accordion/accordion.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+import { AccordionItem } from './accordion-item.interface';
+
+@Component({
+  selector: 'app-accordion',
+  templateUrl: './accordion.component.html',
+  styleUrls: ['./accordion.component.scss'],
+})
+export class AccordionComponent {
+  @Input() items: AccordionItem[] = [];
+
+  toggle(item: AccordionItem): void {
+    item.isExpanded = !item.isExpanded;
+  }
+
+  constructor() {}
+}
