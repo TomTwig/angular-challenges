521f627c6f1412c7f6d2374d8225f80763be346c
Tom Rosenzweig <tom.rosenzweig@macio.de>
2023-11-08 15:47:17 +0100

    delete files

--- challenges/src/app/app.module.ts
+++ challenges/src/app/app.module.ts
@@ -4,12 +4,7 @@ import { AppComponent } from './app.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { CardComponent } from './components/card/card.component';
 import { AccordionComponent } from './components/accordion/accordion.component';
-import { ProgressBarComponent } from './components/progress-bar/progress-bar.component';
-import { StarRatingsComponent } from './components/star-ratings/star-ratings.component';
-import { TopOfPageComponent } from './top-of-page/top-of-page.component';
 import { RouterModule } from '@angular/router';
-import { ComponentsDocumentationComponent } from './components/components-documentation/components-documentation.component';
-import { APP_Routes } from './app.routes';
 
 @NgModule({
   declarations: [
@@ -17,12 +12,8 @@ import { APP_Routes } from './app.routes';
     ToolbarComponent,
     CardComponent,
     AccordionComponent,
-    ProgressBarComponent,
-    StarRatingsComponent,
-    TopOfPageComponent,
-    ComponentsDocumentationComponent,
   ],
-  imports: [BrowserModule, RouterModule.forRoot(APP_Routes)],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- challenges/src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { ComponentsDocumentationComponent } from './components/components-documentation/components-documentation.component';
-
-export const APP_Routes: Routes = [
-  { path: '', component: ComponentsDocumentationComponent },
-];
