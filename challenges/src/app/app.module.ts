9f0d7f36baf398b7850d4f4e9cd9ad531090a25a
Tom Rosenzweig <tom.rosenzweig@macio.de>
2023-11-10 16:06:14 +0100

    add progress bar

--- challenges/src/app/app.module.ts
+++ challenges/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AppComponent } from './app.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { CardComponent } from './components/card/card.component';
 import { AccordionComponent } from './components/accordion/accordion.component';
+import { ProgressBarComponent } from './components/progress-bar/progress-bar.component';
 
 @NgModule({
   declarations: [
@@ -11,6 +12,7 @@ import { AccordionComponent } from './components/accordion/accordion.component';
     ToolbarComponent,
     CardComponent,
     AccordionComponent,
+    ProgressBarComponent,
   ],
   imports: [BrowserModule],
   providers: [],
