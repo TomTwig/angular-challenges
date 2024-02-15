9152b078e836b658d0b8b01626ab83b3a51d9c8c
Tom Rosenzweig <Tom.Rosenzweig@cloudflight.io>
2024-02-15 14:24:54 +0100

    add basic routing

--- challenges/src/app/app.module.ts
+++ challenges/src/app/app.module.ts
@@ -7,6 +7,9 @@ import { AccordionComponent } from './components/accordion/accordion.component';
 import { ProgressBarComponent } from './components/progress-bar/progress-bar.component';
 import { StartRatingsComponent } from './components/start-ratings/start-ratings.component';
 import { TopOfPageComponent } from './components/top-of-page/top-of-page.component';
+import { ROUTES, RouterModule } from '@angular/router';
+import { ComponentDocumentationComponent } from './components/component-documentation/component-documentation.component';
+import { APP_ROUTES } from './app.routes';
 
 @NgModule({
   declarations: [
@@ -17,8 +20,9 @@ import { TopOfPageComponent } from './components/top-of-page/top-of-page.compone
     ProgressBarComponent,
     StartRatingsComponent,
     TopOfPageComponent,
+    ComponentDocumentationComponent,
   ],
-  imports: [BrowserModule],
+  imports: [BrowserModule, RouterModule.forRoot(APP_ROUTES)],
   providers: [],
   bootstrap: [AppComponent],
 })
