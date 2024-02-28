7a1a69e594941e5bfcaeb1c31a43f7baa5b22f6b
Tom Rosenzweig <Tom.Rosenzweig@cloudflight.io>
2024-02-28 14:02:56 +0100

    add creditCardFormatter Pipe

--- challenges/src/app/app.module.ts
+++ challenges/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
-import { CardComponent } from './components/card/card.component';
 import { AccordionComponent } from './components/accordion/accordion.component';
 import { ProgressBarComponent } from './components/progress-bar/progress-bar.component';
 import { StartRatingsComponent } from './components/start-ratings/start-ratings.component';
@@ -10,17 +9,14 @@ import { TopOfPageComponent } from './components/top-of-page/top-of-page.compone
 import { RouterModule } from '@angular/router';
 import { ComponentDocumentationComponent } from './components/component-documentation/component-documentation.component';
 import { APP_ROUTES } from './app.routes';
-
-import { PipesModule } from './pipes/pipes.module';
 import { LoaderComponent } from './components/loader/loader.component';
-import { ServicesDocumentationComponent } from './services/services-documentation/services-documentation.component';
-import { DirectivesDocumentationComponent } from './directives/directives-documentation/directives-documentation.component';
+import { SharedModule } from './shared/shared.module';
+import { PipesModule } from './pipes/pipes.module';
 
 @NgModule({
     declarations: [
         AppComponent,
         ToolbarComponent,
-        CardComponent,
         AccordionComponent,
         ProgressBarComponent,
         StartRatingsComponent,
@@ -28,7 +24,7 @@ import { DirectivesDocumentationComponent } from './directives/directives-docume
         ComponentDocumentationComponent,
         LoaderComponent
     ],
-    imports: [BrowserModule, RouterModule.forRoot(APP_ROUTES), PipesModule],
+    imports: [BrowserModule, RouterModule.forRoot(APP_ROUTES), SharedModule, PipesModule],
     providers: [],
     bootstrap: [AppComponent]
 })
--- challenges/src/app/pipes/pipes.module.ts
+++ challenges/src/app/pipes/pipes.module.ts
@@ -5,9 +5,10 @@ import { PIPES_ROUTES } from './pipes.routes';
 import { TruncatePipe } from './truncate/truncate.pipe';
 import { CreditCardFormatterPipe } from './CreditCardFormatter/credit-card-formatter.pipe';
 import { PipesDocumentationComponent } from './pipes-documentation/pipes-documentation.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
     declarations: [PipesDocumentationComponent, TruncatePipe, CreditCardFormatterPipe],
-    imports: [CommonModule, RouterModule.forChild(PIPES_ROUTES)]
+    imports: [CommonModule, RouterModule.forChild(PIPES_ROUTES), SharedModule]
 })
 export class PipesModule {}
